09b8d1dd4f0a0a6b5f59cd42268b15a88a4e76d3
Maryqueen123 <maryqueenchy@gmail.com>
2022-12-09 15:55:45 -0800

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+int num1, num2;
+
+num1 = 01;
+
+while (num1 < 10)
+{
+	num2 = 01;
+	while (num2 < 10)
+	{
+		putchar(num1);
+		putchar(num2);
+		num2++;
+		putchar(',');
+		putchar(' ');
+	}
+	num1++;
+}
+putchar('\n');
+return (0);
+
+}
