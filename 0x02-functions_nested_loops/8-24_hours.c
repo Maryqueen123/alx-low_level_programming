44239599af9bc14c04659c670001e8889b2dc30a
Maryqueen123 <maryqueenchy@gmail.com>
2022-12-13 08:52:04 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - Prints every minute of the day of
- *		 Jack Bauer, starting from 00:00 to 23:59.
+ *		 Jack Bauer: starting from 00:00 to 23:59.
  */
 
 void jack_bauer(void)
@@ -18,7 +18,7 @@ void jack_bauer(void)
 			_putchar(':');
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
