8e316006642e9e95292e5d282f7adf9d354e5e1d
Maryqueen123 <maryqueenchy@gmail.com>
2023-03-24 05:22:21 -0700

    tasks 2

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc *.c -c -fPIC
-gcc *.o -shared -o liball.so
+gcc -Wall -Werror -Wextra -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LS_LIBRARY_PATH
