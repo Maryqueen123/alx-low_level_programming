421aa7277a4d7537a6b45f73cce4996df2bf52c8
Maryqueen123 <maryqueenchy@gmail.com>
2023-04-06 23:58:37 -0700

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,23 +6,22 @@
  *
  * Return: pointer to the newly created hash table
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ha_tb;
+	hash_table_t *ht;
 	unsigned long int i;
 
-	ha_tb = malloc(sizeof(ha_tb_t));
-	if (ha_tb == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	ha_tb->size = size;
-	ha_tb->array = malloc(size * sizeof(hash_node_t *));
-	if (ha_tb->array == NULL)
-	{
-		free(ha_tb);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
-	}
 	for (i = 0; i < size; i++)
-		ha_tb->array[i] = NULL;
-	return (ha_tb);
+		ht->array[i] = NULL;
+
+	return (ht);
 }
